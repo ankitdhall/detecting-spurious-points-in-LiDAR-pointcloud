ad2f930e9827a18e8b3dbb2a74347e497a60b9f4
ankitdhall <ankitdhall2005@yahoo.co.in>
2018-04-03 12:11:54 +0200

    clean train.py

--- train.py
+++ train.py
@@ -8,12 +8,8 @@ import torch.optim as optim
 
 
 import os
-# from skimage import io, transform
-# import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
-
-# import skimage.transform
 
 import cv2
 
@@ -47,17 +43,8 @@ SAVE_EVERY = 10
 
 GAMMA = 0.0000001
 
-# TRANSFORMS = transforms.Compose([
-# 								 transforms.ToPILImage(),
-# 								 transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.0),
-# 								 transforms.ToTensor()
-# 								 ])
-# TRANSFORMS_TEST = transforms.Compose([
-# 								 transforms.ToPILImage(),
-# 								 transforms.ToTensor()
-# 								 ])
 
-ON_CLUSTER = True
+ON_CLUSTER = False
 
 LIST_PATH = ""
 ANNO_PATH = ""
@@ -98,14 +85,9 @@ testloader = torch.utils.data.DataLoader(dataset=test_dataset,
 RESUME = False
 START_EPOCH = 0
 RESTORE_MODEL_PATH = BKP_DIR + "10.pth"
-# if RESUME:
-# 	print "Resuming training..."
-# 	print "loading model: ", RESTORE_MODEL_PATH
-# 	netA = torch.load(RESTORE_MODEL_PATH)
 
 if RESUME:
 	if os.path.isfile(RESTORE_MODEL_PATH):
-		# print("=> loading checkpoint '{}'".format(args.resume))
 		print("Resuming training...")
 		print("loading model: {}".format(RESTORE_MODEL_PATH))
 		checkpoint = torch.load(RESTORE_MODEL_PATH)
@@ -134,15 +116,9 @@ for epoch in range(START_EPOCH, 502):  # loop over the dataset multiple times
 	running_reg_loss = 0.0
 	# what does enumerate do?
 	for i, data in enumerate(trainloader, 0):
-		# print "minibatch no.:", i
-		# print data
-		# get the inputs
 		inputs, labels = data['image'], data['keypoints']
-		# print inputs.shape
-		# print labels
 
 		# wrap them in Variable
-		# inputs, labels = Variable(inputs), Variable(labels)
 		inputs = Variable(inputs).float()
 		labels = Variable(labels).long()
 		# use torch.cat to concatenate tensors!
@@ -155,9 +131,7 @@ for epoch in range(START_EPOCH, 502):  # loop over the dataset multiple times
 		# forward + backward + optimize
 		outputs = net(inputs)
 
-		# print outputs.shape, labels.shape
 		outputs = outputs.view(outputs.size(0), 2, 16, -1)
-		# print outputs.shape
 
 		data_loss = criterion(outputs, labels)
 		
@@ -188,17 +162,12 @@ for epoch in range(START_EPOCH, 502):  # loop over the dataset multiple times
 				inputs, labels = data['image'], data['keypoints']
 
 				# wrap them in Variable
-				# inputs, labels = Variable(inputs), Variable(labels)
 				inputs = Variable(inputs).float()
 				labels = Variable(labels).long()
-				# use torch.cat to concatenate tensors!
-
-				# inputs = torch.unsqueeze(inputs, 0)
 
 				# forward
 				outputs = net(inputs)
 
-				# print outputs.shape, labels.shape
 				outputs = outputs.view(outputs.size(0), 2, 16, -1)
 				
 				# print torch.max(outputs, 1)
@@ -210,33 +179,19 @@ for epoch in range(START_EPOCH, 502):  # loop over the dataset multiple times
 
 				# only for prining purposes
 				data_loss_test = 0.0
-				
-				# test_loss = test_loss + loss
 
-				# print test_output
-				# print net(torch.unsqueeze(test_input,0))
-
-			# print('[%d, %5d] test loss: %.10f' %
-			# 	  (epoch + 1, i + 1, test_loss*1.0 / len(testloader)))
 			print('[%d, %5d] test loss: %.10f data_loss: %.10f reg_loss: %.10f' %
 				  (epoch + 1, i + 1, test_loss*1.0 / test_sample_count, data_loss_test*1.0 / test_sample_count ,\
 				  						regularization_loss_test*1.0 / test_sample_count))
 			
 			net.train()
 
-	# if epoch%SAVE_EVERY == 0:
-	# 	print("Saving weights...")
-	# 	torch.save(net.state_dict(), BKP_DIR + str(epoch) + ".pth")
 
 	if epoch%SAVE_EVERY == 0:
 		print("Saving weights...")
-		# torch.save(netA.state_dict(), BKP_DIR + str(epoch) + ".pth")
-		# torch.save(netA, BKP_DIR + str(epoch) + ".pth")
 		save_checkpoint({
 			'epoch': epoch + 1,
-			# 'arch': args.arch,
 			'state_dict': net.state_dict(),
-			# 'best_prec1': best_prec1,
 			'optimizer' : optimizer.state_dict(),
 		}, BKP_DIR, str(epoch) + ".pth")
 
